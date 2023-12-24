7773a11da86b59a7c196d6c5b4393f902bd18272
dataCalculus <62516763+dataCalculus@users.noreply.github.com>
2023-12-24 03:46:22 +0300

    Add files via upload

---
+++ INTRO_0.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author: EMIRHAN GURSES
+      * Date: 12.24.2023
+      * Purpose: EDUCATION
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. INTRODUCTION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "IMAGINE A WORLD WITH COBOL"
+            DISPLAY "GOOD BYE FOR NOW"
+            STOP RUN.
+       END PROGRAM INTRODUCTION.
